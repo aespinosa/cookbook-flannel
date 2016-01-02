6469240183c62a989b6d6a6fe05c26407102d77c
Allan Espinosa <allan.espinosa@outlook.com>
2016-01-01 18:55:35 -0600

    Add cookbook information

--- metadata.rb
+++ metadata.rb
@@ -1,1 +1,8 @@
 name 'flannel'
+maintainer 'Allan Espinosa'
+maintainer_email 'allan.espinosa@outlook.com'
+source_url 'https://github.com/aespinosa/cookbook-flannel'
+issues_url 'https://github.com/aespinosa/cookbook-flannel/issues'
+
+
+supports 'debian'
