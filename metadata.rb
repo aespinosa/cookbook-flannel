82a5694a31d767c8aa288ca21a0a381cc214ad58
Allan Espinosa <allan.espinosa@outlook.com>
2016-01-01 18:55:50 -0600

    Bump and release first cut as 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,6 @@ maintainer_email 'allan.espinosa@outlook.com'
 source_url 'https://github.com/aespinosa/cookbook-flannel'
 issues_url 'https://github.com/aespinosa/cookbook-flannel/issues'
 
+version '0.1.0'
 
 supports 'debian'
