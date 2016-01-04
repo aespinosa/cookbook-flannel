ed428ceee9f8aaee3dd436be0036d9a4eee08f26
Allan Espinosa <allan.espinosa@outlook.com>
2016-01-03 23:44:47 -0600

    Bump metadata and cut as 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name 'flannel'
+description 'A library cookbook to setup flanneld'
 maintainer 'Allan Espinosa'
 maintainer_email 'allan.espinosa@outlook.com'
 source_url 'https://github.com/aespinosa/cookbook-flannel'
 issues_url 'https://github.com/aespinosa/cookbook-flannel/issues'
 
-version '0.1.0'
+version '0.2.0'
 
 supports 'debian'
