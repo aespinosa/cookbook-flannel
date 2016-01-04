b1647b9c6707db69cbb5bd1056e5608cd2cd4755
Allan Espinosa <allan.espinosa@outlook.com>
2016-01-04 00:00:10 -0600

    Add license information to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 name 'flannel'
 description 'A library cookbook to setup flanneld'
 maintainer 'Allan Espinosa'
+license 'Apache v2.0'
 maintainer_email 'allan.espinosa@outlook.com'
 source_url 'https://github.com/aespinosa/cookbook-flannel'
 issues_url 'https://github.com/aespinosa/cookbook-flannel/issues'
