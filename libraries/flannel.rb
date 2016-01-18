c2ec29db15ad0f26a41029be7bd158c98552b78c
Allan Espinosa <allan.espinosa@outlook.com>
2016-01-17 19:17:40 -0600

    Split long string and regex lines

--- libraries/flannel.rb
+++ libraries/flannel.rb
@@ -8,7 +8,8 @@ module FlannelCookbook
     # Reference: https://github.com/coreos/flannel#configuration
     property :configuration, Hash, default: { 'Network' => '10.0.0.1/8' }
 
-    IPV4_ADDR ||= /((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])/
+    IPV4_ADDR ||= /((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.)
+                   {3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])/x
 
     # Reference: https://github.com/coreos/flannel#key-command-line-options
     property :public_ip, [IPV4_ADDR, nil]
@@ -29,15 +30,18 @@ module FlannelCookbook
     alias_method :v, :log_level
 
     action :create do
-      
+
       remote_file 'flannel tarball' do
         path "#{file_cache_path}/flannel-0.5.5-linux-amd64.tar.gz"
-        source 'https://github.com/coreos/flannel/releases/download/v0.5.5/flannel-0.5.5-linux-amd64.tar.gz'
-        checksum '78127e165f124a8f8ddf2391f8b775b3494aec7e693928f78f2328256fe3644c'
+        source 'https://github.com/coreos/flannel/releases/download/v0.5.5'\
+               '/flannel-0.5.5-linux-amd64.tar.gz'
+        checksum '78127e165f124a8f8ddf2391f8b775b3'\
+                 '494aec7e693928f78f2328256fe3644c'
       end
 
       execute 'extract flanneld' do
-        command "tar xvf #{tarball_path} -C #{flanneld_bin_prefix} flannel-0.5.5/flanneld --strip-components=1"
+        command "tar xvf #{tarball_path} -C #{flanneld_bin_prefix} "\
+                'flannel-0.5.5/flanneld --strip-components=1'
         action :nothing
         subscribes :run, 'remote_file[flannel tarball]'
       end
